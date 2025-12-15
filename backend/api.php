27c9484eab5e002fa8f9aafbb361daeae17fde1f
BaoNewbie128 <binkongu24@gmail.com>
2025-12-15 22:16:21 +0700

    Init HTML PHP project

---
+++ backend/api.php
@@ -0,0 +1,5 @@
+<?php
+header("Content-Type: application/json");
+echo json_encode([
+  "message" => "Backend PHP + MySQL OK"
+]);
---
+++ backend/config.php
@@ -0,0 +1,5 @@
+<?php
+$conn = new mysqli("localhost", "DB_USER", "DB_PASS", "DB_NAME");
+if ($conn->connect_error) {
+    die("DB error");
+}
